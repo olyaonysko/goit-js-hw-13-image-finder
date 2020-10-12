9f5bfdc97fcd3554ba24fbc45030b1fae00e823c
Olya <olya.onysko@gmail.com>
2020-10-12 19:54:55 +0300

    fix some bag

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import imagesService from './js/images-service';
 import updateMarkup from './js/update-markup';
 import onOpenHandler from './js/modal';
 import loadMoreBtn from './js/components/load-more-btn';
+import { error } from '@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 import refs from './js/refs';
 
 const loadMoreButton = new loadMoreBtn({
@@ -18,6 +21,13 @@ function searchFormSubmitHandler(event) {
   event.preventDefault();
   const form = event.currentTarget;
   imagesService.query = form.elements.query.value;
+  if (!imagesService.query | imagesService.query === " ") {
+     error({
+        text: 'The field is empty.',
+     });
+
+    return;
+  }
   clearContainer();
   imagesService.resetPage();
   fetchImages();
@@ -28,6 +38,12 @@ function fetchImages() {
   loadMoreButton.disable();
 
   imagesService.searchImages().then(images => {
+    if (images.length === 0) {
+       error({
+        text: 'Cannot find images. Try again.',
+       });
+      loadMoreBtn.hide()
+    }
     updateMarkup(images);
     loadMoreButton.show();
     loadMoreButton.enable();
